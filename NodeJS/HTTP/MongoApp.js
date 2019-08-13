dca2cad3093426d2972dbe9e69e45e63b7911436
Mayur Wadibhasme <mayur.wadibhasme@publicissapient.com>
2019-08-13 17:02:38 +0530

    web works till nodejs

--- NodeJS/HTTP/MongoApp.js
+++ NodeJS/HTTP/MongoApp.js
@@ -1,45 +1,52 @@
 const express = require('express');
+const bodyparser = require('body-parser')
 const {MongoClient, ObjectId} = require('mongodb');
 const url = "mongodb://localhost:27017"
-const {getEmployeeById,getSortedRecords} = require("../services/mongo-emps-service")
+const {getEmployeeById,getSortedRecords,addNewEmp} = require("../services/mongo-emps-service")
 const id = "5d43cd30a6d183f707172497"
-
 
 const app = express();
 
-app.get("/:id", (req, res) => {
-
+app.use(bodyparser.json());
+app.get("/employee/:id", (req, res) => {
+    
     getEmployeeById(req.params.id)
     .then(data => res.send(data))
-    .catch(err => console.log(err)
-    )
-
+    .catch(err => console.log(err))
+    
 })
 
 app.get("/employee", (req,res) => {
-
+    
     getSortedRecords()
     .then(data => res.send(data))
     .catch(err => console.log(err))
-
+    
 })
 
+app.post("/employee", (req,res) => {
+    
+    addNewEmp(req.body)
+    .then(data => res.send(data))
+    .catch(err => console.log(err))
+})
 
 //to show path param
 // app.get("/example/:name/:age",(req,res) => {
-//     //https://localhost:3000/example/Mayur/29
-//     console.log("--------------------------");
-//     console.log("Name: " + req.params.name);
-//     console.log("Age: " + req.params.age);
-//     console.log("--------------------------");
+    //     //https://localhost:3000/example/Mayur/29
+    //     console.log("--------------------------");
+    //     console.log("Name: " + req.params.name);
+    //     console.log("Age: " + req.params.age);
+    //     console.log("--------------------------");
     
-
-//     console.log(req.query);
-//     //to invoke query params
-//     //    //https://localhost:3000/example?city=Bengaluru&state=karnataka
     
-//     res.send("State " + req.query.state)
-
+    //     console.log(req.query);
+    //     //to invoke query params
+    //     //    //https://localhost:3000/example?city=Bengaluru&state=karnataka
     
-// })
+    //     res.send("State " + req.query.state)
+    
+    
+    // })
+    
 app.listen(3000)
